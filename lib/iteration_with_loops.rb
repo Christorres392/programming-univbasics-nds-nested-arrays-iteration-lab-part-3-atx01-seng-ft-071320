d4e0922283a6e2e6621731e71697a466323f31f0
Christopher Torres <christorres09@gmail.com>
2020-07-02 01:04:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index] do
     if src[row_index][element_index].class == String
-      new_string += src[row_index][element_index] + ' '
+      new_string += src[row_index][element_index] + " "
     end
     row_index += 1
   end
